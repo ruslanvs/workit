1deba170e224f0dde151a992bc696cdf7649d28b
ccasil <cesarkylecasil@gmail.com>
2018-03-08 15:56:09 -0800

    added base code/ initial commit

--- workit/ViewController.swift
+++ workit/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    var motionManager = CMMotionManager()
+    let opQueue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if motionManager.isDeviceMotionAvailable {
+            print("We can detect device motion")
+            startReadingMotionData()
+        }
+        else {
+            print("We cannot detect device motion")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func startReadingMotionData() {
+        // set read speed
+        motionManager.deviceMotionUpdateInterval = 1
+        // start reading
+        motionManager.startDeviceMotionUpdates(to: opQueue) {
+            (data: CMDeviceMotion?, error: Error?) in
+            
+            if let mydata = data {
+                print("mydata", mydata.gravity)
+                //                print("pitch raw", mydata.attitude.pitch)
+                //                print("pitch", self.degrees(mydata.attitude.pitch))
+            }
+        }
+    }
+    
+    
+    func degrees(_ radians: Double) -> Double {
+        return 180/Double.pi * radians
+    }
+    
 }
-
